21ce5d093bf92a6b1687916be28d26d5ca565e0f
Nuno Louro <nlouro@student.42heilbronn.de>
2021-09-10 14:06:21 +0200

    substr working

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 11:30:02 by nlouro            #+#    #+#             */
-/*   Updated: 2021/09/10 11:56:16 by nlouro           ###   ########.fr       */
+/*   Updated: 2021/09/10 14:04:25 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*buffer;
 	int		i;
 
+	if (s == 0)
+		return (0);
+	if ((int)start >= ft_strlen(s))
+	{
+		buffer = ft_calloc(1, 1);
+		if (buffer == 0)
+			return (NULL);
+		return (buffer);
+	}
 	buffer = malloc(len + 1);
 	if (buffer == 0)
 		return (NULL);
@@ -26,6 +35,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		buffer[i] = *(s + start + i);
 		i++;
 	}
-	buffer[i + 1] = '\0';
+	buffer[i] = '\0';
 	return (buffer);
 }
