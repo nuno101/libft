a2ca3cbfcca96cec2354e0723954733928c37a45
Nuno Louro <nlouro@student.42heilbronn.de>
2021-09-10 18:21:48 +0200

    strtrim working

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 11:08:17 by nlouro            #+#    #+#             */
-/*   Updated: 2021/09/10 15:18:23 by nlouro           ###   ########.fr       */
+/*   Updated: 2021/09/10 17:35:47 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,5 +48,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 /* Additional functions */
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 
 #endif
