824599c5e5f03c7f553de84216d4be740053940d
Nuno <nuno@innoflair.com>
2021-09-15 10:01:20 +0200

    bugfixes and rewrite of ft_strnstr

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 17:00:34 by nlouro            #+#    #+#             */
-/*   Updated: 2021/08/26 14:20:41 by nlouro           ###   ########.fr       */
+/*   Updated: 2021/09/15 09:06:04 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
+	if (n == 0)
+		return (0);
 	i = 0;
 	while ((*s1 != '\0' && *s2 != '\0') && i + 1 < n && *s1 == *s2)
 	{
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 11:47:45 by nlouro            #+#    #+#             */
-/*   Updated: 2021/09/10 10:40:23 by nlouro           ###   ########.fr       */
+/*   Updated: 2021/09/15 10:00:15 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,26 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	size_t	match_len;
 
+	if (len == 0)
+		return (0);
+	if (ft_strlen(needle) == 0)
+		return ((char *)haystack);
 	i = 0;
 	j = 0;
-	match_len = ft_strlen(needle);
-	if (match_len == 0)
-		return ((char *)haystack);
-	while (*(haystack + i + j) != '\0' && (i + j) < len)
+	while (haystack[i] != '\0' && i < len)
 	{
-		if (*(haystack + i + j) == *(needle + i + j) && (i + j) < len)
+		if (haystack[i] == needle[j])
 		{
-			match_len--;
-			if (match_len == 0)
-				return ((char *)(haystack + i));
 			j++;
+			if (needle[j] == '\0' || i >= len)
+				return ((char *)(haystack + i - j + 1));
 		}
 		else
 		{
-			i = i + j + 1;
 			j = 0;
-			match_len = ft_strlen(needle);
 		}
+		i++;
 	}
 	return (NULL);
 }
