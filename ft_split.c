881d658920381280d61feb033b43bba0c9f482ec
Nuno Louro <nlouro@student.42heilbronn.de>
2021-09-16 12:01:33 +0200

    WIP

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 18:23:34 by nlouro            #+#    #+#             */
-/*   Updated: 2021/09/10 18:35:31 by nlouro           ###   ########.fr       */
+/*   Updated: 2021/09/16 11:01:55 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,25 @@
 
 char	**ft_split(char const *s, char c)
 {
-	//char **array;
+	int len;
+	int i;
+	int count;
+	char *array[];
+
+	len = ft_strlen(s);
 	ft_strchr(s, c);
+	if (s == 0)
+		return (0); 
+	i = 0;
+	count = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			*array[count] = 
+			count++;
+		}
+		i++
+	}
 	return (0); 
 }
