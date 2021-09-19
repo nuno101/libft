63d8573035bcae45506dfc40f6b1722cb802a731
Nuno <nuno@innoflair.com>
2021-09-19 22:37:57 +0200

    ft_split wip

--- ft_split.c
+++ ft_split.c
@@ -6,33 +6,81 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 18:23:34 by nlouro            #+#    #+#             */
-/*   Updated: 2021/09/16 11:01:55 by nlouro           ###   ########.fr       */
+/*   Updated: 2021/09/19 22:36:07 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+ * int array_len - count the number of c separators + 1. Add 1 for NULL array terminator
+ * char *str - copy of *s const argument
+ * *words_len - array storing the length of each word
+ */
+
+int	count_words(const char *str, char c)
+{
+	int wc;
+	int i;
+	int len;
+
+	wc = 0;
+	i = 0;
+	len = ft_strlen(str);
+	while (i < len) 
+	{
+		if (str[i] == c)
+			if ((i > 0 && str[i - 1] != c) && (i + 1 < len && str[i + 1] != '\0'))
+				wc++;
+		i++;
+	}
+	return (wc);
+}
+
+char	**split_string(const char *str, char c)
+{
+	char **result;
+    int i;
+	int wc;
+	int wbegin;
+	int wend;
+
+	wc = count_words(str, c);
+	result = (char **)malloc((wc + 1) * sizeof(char *));
+	if (result == 0)
+		return (0);
+	i = 0;
+	wbegin = 0;
+	wend = 0;
+	while (i < wc && str[wend] != '\0')
+	{
+		while (str[wend] == c)
+			wend++;
+		wbegin = wend;
+		while (str[wend] != c)
+			wend++;
+		result[i] = ft_substr(str, wbegin, wend - wbegin);
+	}
+	result[i] = 0;
+	return (result); 
+}
+
 char	**ft_split(char const *s, char c)
 {
-	int len;
 	int i;
-	int count;
-	char *array[];
+	char **result;
 
-	len = ft_strlen(s);
-	ft_strchr(s, c);
 	if (s == 0)
 		return (0); 
 	i = 0;
-	count = 0;
-	while (s[i] != '\0')
+	while (s[i] == c)
+		i++;
+	if (s[0] == '\0' || *ft_strchr(s, c) || i == ft_strlen(s)) 
 	{
-		if (s[i] == c)
-		{
-			*array[count] = 
-			count++;
-		}
-		i++
+		result = malloc(1 * sizeof(char *));
+		result[0] = NULL;
+		return (result); 
 	}
-	return (0); 
+	result = split_string(s, c);
+	return (result);
 }
