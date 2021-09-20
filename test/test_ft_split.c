267194a0351f6d328fdce17d8bd094a9b3e570b4
Nuno <nuno@innoflair.com>
2021-09-20 23:25:14 +0200

    one more ft_split test

--- test/test_ft_split.c
+++ test/test_ft_split.c
@@ -36,4 +36,17 @@ int main()
   printf("Result string 2 is: %s\n", result4[2]);
   printf("Result string 3 is: %s\n", result4[3]);
 
+  char str5[] = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse";
+  printf("Test string is: %s\n", str5);
+  printf("Test string has #words: %d.\n", count_words(str5, c));
+  char **result5 = ft_split(str5, c);
+  printf("Result string 0 is: %s\n", result5[0]);
+  printf("Result string 1 is: %s\n", result5[1]);
+  printf("Result string 2 is: %s\n", result5[2]);
+  printf("Result string 3 is: %s\n", result5[3]);
+  printf("Result string 7 is: %s\n", result5[7]);
+  printf("Result string 10 is: %s\n", result5[10]);
+  printf("Result string 11 is: %s\n", result5[11]);
+  printf("Result string 12 is: %s\n", result5[12]);
+
 }
