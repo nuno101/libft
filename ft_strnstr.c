e9772a589bee5f3a9852968e9ae3f9b65222b7e2
Nuno <nuno@innoflair.com>
2021-09-21 23:44:47 +0200

    Bugfix in strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 11:47:45 by nlouro            #+#    #+#             */
-/*   Updated: 2021/09/15 10:00:15 by nlouro           ###   ########.fr       */
+/*   Updated: 2021/09/21 23:41:17 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	i;
 	size_t	j;
 
-	if (len == 0)
-		return (0);
 	if (ft_strlen(needle) == 0)
 		return ((char *)haystack);
+	if (len == 0)
+		return (0);
 	i = 0;
 	j = 0;
 	while (haystack[i] != '\0' && i < len)
@@ -34,6 +34,8 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		else
 		{
 			j = 0;
+			if (haystack[i] == needle[j])
+				j++;
 		}
 		i++;
 	}
