294c7abb20b6e46138e9a3b3741f8b4f1aff5215
Nuno Louro <nlouro@student.42heilbronn.de>
2021-09-23 15:08:08 +0200

    ft_split working and norm compliant

--- ft_split.c
+++ ft_split.c
@@ -6,24 +6,20 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 18:23:34 by nlouro            #+#    #+#             */
-/*   Updated: 2021/09/23 14:50:01 by nlouro           ###   ########.fr       */
+/*   Updated: 2021/09/23 15:03:46 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*
- *
- */
-
 int	count_words(const char *s, char c)
 {
-	int wc;
-	int i;
+	int	wc;
+	int	i;
 
 	wc = 0;
 	i = 0;
-	while (s[i] !=  '\0') 
+	while (s[i] != '\0')
 	{
 		if ((i == 0 || *(s + i) == c) && *(s + i + 1) != c && *(s + i + 1) != 0)
 			wc++;
@@ -36,18 +32,13 @@ int	count_words(const char *s, char c)
 
 char	**split_string(const char *str, char c)
 {
-	char **result;
-    int i;
-	int len;
-	int wc;
-	int wbegin;
-	int wend;
+	char	**result;
+	int		i;
+	int		wc;
+	int		wbegin;
+	int		wend;
 
 	wc = count_words(str, c);
-	len = ft_strlen(str);
-    //printf("DEBUG: Test string [%s] has #words: %d.\n", str, wc);
-    //printf("DEBUG: Test string [%s] has length: %d.\n", str, ft_strlen(str));
-
 	result = (char **)malloc((wc + 1) * sizeof(char *));
 	if (result == 0)
 		return (0);
@@ -58,45 +49,35 @@ char	**split_string(const char *str, char c)
 	{
 		while (str[wend] == c)
 			wend++;
-    	//printf("DEBUG 10: wend value s: %d.\n", wend);
 		wbegin = wend;
-    	//printf("DEBUG 20: wbegin value s: %d.\n", wbegin);
-		while (str[wend] != c && wend < len)
-		{
-    		//printf("DEBUG 30: str[wend] value is: %c.\n", str[wend]);
+		while (str[wend] != c && wend < ft_strlen(str))
 			wend++;
-		}
-    	//printf("DEBUG 30: wend value s: %d.\n", wend);
 		result[i] = ft_substr(str, wbegin, wend - wbegin);
 		i++;
 	}
 	result[i] = NULL;
-	return (result); 
+	return (result);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	int i;
-	char **result;
+	int		i;
+	char	**result;
 
 	if (s == 0)
-		return (0); 
+		return (0);
 	if (s[0] == 0)
 		return (ft_calloc(1, sizeof(char *)));
 	i = 0;
-   	//printf("DEBUG 1: i: %d.\n", i);
-	while (s[i] == c )
+	while (s[i] == c)
 		i++;
-   	//printf("DEBUG 2: i: %d.\n", i);
-	//if (s[0] == '\0' || (strchr_result == NULL) || i == ft_strlen(s)) 
-	if (s[0] == '\0' || i == ft_strlen(s)) 
+	if (s[0] == '\0' || i == ft_strlen(s))
 	{
-    	//printf("DEBUG 3 : %d.\n", i);
 		result = ft_calloc(1, sizeof(char *));
 		if (result == 0)
 			return (0);
 		result[0] = NULL;
-		return (result); 
+		return (result);
 	}
 	result = split_string(s, c);
 	return (result);
